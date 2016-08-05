528c3dca9a81b912630d68dfb4efe1de817265d5
Alex Moore <alexander.g.moore1@gmail.com>
2016-08-04 21:10:54 -0600

    Change width to always be full width

--- webform-bootstrap-style.js
+++ webform-bootstrap-style.js
@@ -8,7 +8,7 @@ try {
         $('head').append('<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">');
         $('head').append('<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css" />');
         $('form').eq(0).wrap('<div id="content" class="container" />');
-        $('form').eq(0).wrap('<div class="col-xs-12 col-sm-6 col-sm-offset-3" />');
+        $('form').eq(0).wrap('<div class="col-xs-12 col-sm-12" />');
         $('.infusion-field-input-container').removeAttr('style');
         $(':input.infusion-field-input-container').each(function (i) {
             var self = $(this);
@@ -29,7 +29,10 @@ try {
             childTD.remove();
         });
         $('#inf_field_FirstName').attr({
-            placeholder: 'Enter your first name'
+            placeholder: 'Enter your First Name'
+        });
+        $('#inf_field_LastName').attr({
+            placeholder: 'Enter your Last Name'
         });
         $('#inf_field_Email').attr({
             type: 'email',
